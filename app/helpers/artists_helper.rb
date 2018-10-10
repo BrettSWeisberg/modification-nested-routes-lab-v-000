7ae1ba829a2423e2405d9adc7f0e21bcc789913e
Brett Weisberg <weisberg.brett@gmail.com>
2018-10-10 01:07:34 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -5,7 +5,7 @@ module ArtistsHelper
 
   def artist_select(artist, song)
 
-  if artist
+  if !artist
     select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
   else
     hidden_field_tag "song[artist_id]", song.artist_id
